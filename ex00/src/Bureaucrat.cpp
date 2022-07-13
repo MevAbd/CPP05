0237c5add97047fa69058c09e176e3d888aeed92
Maeva ALBRAND <malbrand@e1r4p16.clusters.42paris.fr>
2022-07-13 17:30:59 +0200

    ex00

---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 10:54:19 by malbrand          #+#    #+#             */
+/*   Updated: 2022/07/13 17:05:21 by malbrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.class.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name) 
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade = grade;
+	std::cout << _name << " constructor called" << std::endl;
+	return ;
+}
+
+Bureaucrat::Bureaucrat(void) : _name("No_Name"), _grade(150)
+{
+	std::cout << _name << " constructor called" << std::endl;
+	return ;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &rhs) : _name("No_Name")
+{
+	*this = rhs;
+	std::cout << this->_name << " copy constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	std::cout << this->_name << " destructor called" << std::endl;
+	return ;
+}
+
+void	Bureaucrat::upGrade(void)
+{
+	if (this->_grade > 1)
+		this->_grade--;
+	else
+		std::cout << "You can't up grade " << _name << ", grade is too high." << std::endl;
+	return ;
+}
+
+void	Bureaucrat::downGrade(void)
+{
+	if (this->_grade < 150)
+		this->_grade++;
+	else
+		std::cout << "You can't down grade " << _name << ", grade is too low." << std::endl;
+	return ;
+}
+
+std::string	Bureaucrat::getName(void) const
+{
+	std::string	ret;
+
+	ret = this->_name;
+	return (ret);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+std::ostream	&operator<<(std::ostream &o, const Bureaucrat &bureaucrat)
+{
+	o << bureaucrat.getName() << ", grade " << bureaucrat.getGrade();
+	return (o);
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	this->_grade = rhs._grade;
+	return (*this);
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 11:58:54 by malbrand          #+#    #+#             */
+/*   Updated: 2022/07/13 17:29:48 by malbrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.class.hpp"
+
+void	instantiate(void)
+{
+	Bureaucrat	lala("lala", 6);
+	std::cout << std::endl;
+
+	Bureaucrat	test;
+	std::cout << std::endl;
+
+	Bureaucrat	lalo(lala);
+	std::cout << std::endl;
+
+	std::cout << lala.getName() << std::endl;
+	std::cout << lala.getGrade() << std::endl;
+	std::cout << test.getName() << std::endl;
+	std::cout << test.getGrade() << std::endl;
+	std::cout << lalo.getName() << std::endl;
+	std::cout << lalo.getGrade() << std::endl;
+}
+
+void	moremore(void)
+{
+	Bureaucrat	max("MAX", 5);
+	std::cout << std::endl;
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << max << std::endl;
+		max.upGrade();
+	}
+	std::cout << std::endl;
+}
+
+void	lessless(void)
+{
+	Bureaucrat	min("MIN", 146);
+	std::cout << std::endl;
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << min << std::endl;
+		min.downGrade();
+	}
+	std::cout << std::endl;
+}
+
+void	moreThanMax(void)
+{	
+	Bureaucrat	maxmax("MAXMAX", -6);
+}
+
+void	lessThanMin(void)
+{	
+	Bureaucrat	minmin("MINMIN", 169);
+}
+int	main(void)
+{
+	try
+	{
+		instantiate();	
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try
+	{
+		moremore();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try
+	{
+		lessless();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try
+	{
+		moreThanMax();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try
+	{
+		lessThanMin();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
